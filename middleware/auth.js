dad69fec77dd04eb817f989b16a14d54feda3edc
Mvcampbell3 <mvcampbell3@gmail.com>
2020-09-06 14:23:21 -0400

    cleaned up auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
   try {
-    console.log(req.headers)
     const token = req.headers["authorization"].split(' ')[1];
-    console.log(token)
     const auth = jwt.verify(token, process.env.JWT_KEY);
-    console.log(auth);
     next();
   } catch (error) {
     console.log(error)
