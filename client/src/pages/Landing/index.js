1aeeda30e2fef9c52e562699de90542ce04dff33
Mvcampbell3 <mvcampbell3@gmail.com>
2020-09-06 21:05:40 -0400

    landing page content and style start

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -9,12 +9,16 @@ const Landing = (props) => {
       <div className="main">
         <div className="splash">
           <div className="splash-wrapper">
-            <h2 className="splash-title tech opac-up">Cutting Edge</h2>
-            <p className="splash-sub ofsp opac-up">Office Space Store</p>
+            <h2 className="splash-title tech opac-up">Welcome to Initech</h2>
+            <button className="btn splash-btn tech">Visit Store</button>
           </div>
         </div>
-        <button className="btn" onClick={props.loginUser}>Login</button>
-        <button className="btn" onClick={props.checkAuth}>Check</button>
+        <section className="message">
+          <div className="message-wrapper">
+            <h2 className="message-title norm">Disclaimer</h2>
+            <p className="message-content norm">- This website is not a real online store! This is a demonstration of React.js, Stripe.js, and other various web development technologies. It is very important to me that you know this. The payment area does not even accept real credit/debit cards! There is a list of multiple test cards that you can use, and they will be displayed on the payment screen.</p>
+          </div>
+        </section>
       </div>
     </div>
   );
