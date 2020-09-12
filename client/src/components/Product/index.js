cf6bdf40dd9584a25a870437b0b40e097eed06c8
Mvcampbell3 <mvcampbell3@gmail.com>
2020-09-12 15:53:14 -0400

    product style revamp

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -17,23 +17,30 @@ const Product = (props) => {
   return (
     <div className="product">
       <div className="product-wrapper">
-        <div className="product-bg"></div>
-        <div className="product-label">
-          <h2 className="tech product-title">{product.name}</h2>
-        </div>
         <div className="product-body">
-          <img className='product-img' src={`./assets/images/products/${product.img_path}`} alt={product.name} />
           <div className="product-info">
-            <p className="product-text tech">Info: <span className='data tech'>{product.description}</span></p>
-            <p className="product-text tech">Price: <span className='data tech'>${formPrice}</span></p>
-            <p className="product-text tech">Stock: <span className='data tech'>{product.quantity}</span></p>
-            <div className="action-area">
-              <div className="input-product">
-                <label htmlFor='input' className="my-label tech">Add to cart</label>
-                <input type="number" className="my-number-input" value={addNumber} onChange={(e) => setAddNumber(e.target.value)} />
-                <button className="my-input-button"><i className="fas fa-plus"></i></button>
+            <div className="info-top">
+              <img className='product-img' src={`./assets/images/products/${product.img_path}`} alt={product.name} />
+              <h2 className="tech product-title">{product.name}</h2>
+
+              <p className="product-text tech"><span className='data tech'>{product.description}</span></p>
+
+            </div>
+
+            <div className="info-bottom">
+              <div className="stats flex-between">
+                <p className="product-text tech">Price: <span className='data tech'>${formPrice}</span></p>
+                <p className="product-text tech">Stock: <span className='data tech'>{product.quantity}</span></p>
+              </div>
+              <div className="action-area">
+                <div className="input-product">
+                  <label htmlFor='input' className="my-label tech">Add to cart</label>
+                  <input type="number" className="my-number-input" value={addNumber} onChange={(e) => setAddNumber(e.target.value)} />
+                  <button className="my-input-button"><i className="fas fa-plus"></i></button>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
